14cc10d73201888a08ac4d18b554aff8f9324bed
fadiyahyaoui <yahyaouifadi@gmail.com>
2024-03-03 13:54:24 +0100

    Your commit message here

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ bot.on('text', (msg) => {
               );
               const roundedAzimuth = Math.round(azimuth);
 
-              bot.sendMessage(chatId, `Secteur: ${entry.sector}\nPCI: ${entry.PCI}\n${entry.distance} meters\nAzimuth: ${roundedAzimuth}°`);
+              bot.sendMessage(chatId, `Secteur: ${entry.site}\n **${entry.distance} meters**\nAzimuth: ${roundedAzimuth}°`);
             });
           });
 
